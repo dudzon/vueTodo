2617227154e0d676ca90a9bf442a4c91b1c632bd
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2019-03-03 09:16:04 +0100

    App config, installed additional depedencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbvue/build/css/mdb.css";
+import Vue from "vue";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
